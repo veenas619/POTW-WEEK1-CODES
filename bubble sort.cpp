5ec03e4dab3be2dd04d1f01e992794e0eebe71fd
veenas619 <veenashastri2006@gmail.com>
2025-11-29 00:00:53 +0530

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'countSwaps' function below.
+ *
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+void countSwaps(vector<int> a) {
+    int swaps=0;
+    for(int p=0;p<a.size();p++)
+    {
+        for(int c=0;c<a.size()-p-1;c++)
+        {
+            if(a[c]>a[c+1])
+            {   swaps++;
+                int t=a[c];
+                a[c]=a[c+1];
+                a[c+1]=t;
+            }
+        }
+    }
+    cout<<"Array is sorted in "<<swaps<<" swaps."<<endl;
+    cout<<"First Element: "<<a[0]<<endl;
+    cout<<"Last Element: "<<a[a.size()-1]<<endl;
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
